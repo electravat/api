81732050dc42eea43f8ac067bcf67f09fecad7ff
MwSpace LLC <alex@mwspace.com>
2019-01-16 14:10:18 +0100

    Up

---
+++ src/Api.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Silence is Gold
+ */
+
+namespace Electravat\Api;
+
+/**
+ * Class Api
+ * @package Electravat\Api
+ */
+class Api
+{
+    /**
+     * Api constructor.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
